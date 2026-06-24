3e3ace438d9f0a36131938ba140caefbfea5d8f5
lhjjhg <lhjjhg@naver.com>
2026-06-25 00:31:10 +0900

    feat: 백엔드 스프링 부트 초기 세팅

---
+++ backend/src/main/java/com/wandermap/wandermap/WandermapApplication.java
@@ -0,0 +1,13 @@
+package com.wandermap.wandermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WandermapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WandermapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wandermap/wandermap/WandermapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandermap.wandermap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WandermapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
